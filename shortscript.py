38602c1bbab6ed5741118dffc8a11179b6f8bbda
imjayapple <imjayapple@gmail.com>
2024-02-06 22:07:03 -0800

    Create template document, add comments

---
+++ shortscript.py
@@ -0,0 +1,16 @@
+# Set the download path to './' to get the video in the current directory
+# Use Relative Path to download to keep downloaded files out of the project
+
+from pytube import YouTube
+
+url = "" #Put URL of YouTube Video or YouTube Short here
+
+yt = YouTube(url)
+
+stream = yt.streams.get_highest_resolution()
+
+download_path = 'S:\PERSONAL\2024\Screen Recording'
+
+stream.download(output_path=download_path)
+
+print(f"Downloaded '{yt.title}' to {download_path}")
